e371524dc452115a86a723f8a48a87f06540fc8c
liucheng <790809568@qq.com>
2020-03-24 20:15:26 +0800

    mqtt

--- src/main/java/com/pi/client/pi_client/ApplicationContext.java
+++ src/main/java/com/pi/client/pi_client/ApplicationContext.java
@@ -3,7 +3,6 @@ package com.pi.client.pi_client;
 import com.pi.client.pi_client.service.HttpService;
 import com.pi.client.pi_client.service.MqttService;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServer;
 import lombok.Data;
 
 @Data
