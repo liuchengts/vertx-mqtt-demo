e4ab7280e40f9f7be72c36adf448676a274481a8
liucheng <790809568@qq.com>
2020-03-24 18:27:15 +0800

    mqtt

--- src/main/java/com/pi/client/pi_client/PiApplication.java
+++ src/main/java/com/pi/client/pi_client/PiApplication.java
@@ -2,14 +2,20 @@ package com.pi.client.pi_client;
 
 import com.pi.client.pi_client.handles.HttpService;
 import com.pi.client.pi_client.handles.MqttService;
+import com.pi.client.pi_client.model.ResponseDTO;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.Json;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class PiApplication extends AbstractVerticle {
-//  public static void main(String[] args) {
-//    service();
-//  }
+  public static void main(String[] args) {
+    service();
+  }
 
   @Override
   public void start() throws Exception {
@@ -21,6 +27,11 @@ public class PiApplication extends AbstractVerticle {
       HttpService.start();
     } catch (Exception e) {
       log.error("HttpService error", e);
+    }
+    try {
+      MqttService.start();
+    } catch (Exception e) {
+      log.error("MqttService error", e);
     }
   }
 }
--- src/main/java/com/pi/client/pi_client/handles/MqttService.java
+++ src/main/java/com/pi/client/pi_client/handles/MqttService.java
@@ -9,21 +9,31 @@ import io.vertx.core.buffer.Buffer;
 @Slf4j
 public class MqttService {
   static MqttClient client;
+  static Vertx vertx = Vertx.vertx();
 
   public static void start() {
-    client = MqttClient.create(Vertx.vertx());
-    client.connect(1883, "mqtt.ayouran.com", s -> {
-      client.disconnect();
-    });
-    client.publishHandler(s -> {
-      log.info("There are new message in topic: " + s.topicName());
-      log.info("Content(as string) of the message: " + s.payload().toString());
-      log.info("QoS: " + s.qosLevel());
-    }).subscribe("lot-admin", MqttQoS.AT_LEAST_ONCE.value());
+    client = MqttClient.create(vertx);
+    client.connect(1883, "mqtt.ayouran.com", c -> {
+      if (c.succeeded()) {
+        log.info("Connected to a server");
+        client.subscribe("lot-admin", MqttQoS.AT_LEAST_ONCE.value());
+      } else {
+        log.error("Failed to connect to a server");
+        log.error("error", c.cause());
+      }
+    })
+      .subscribeCompletionHandler(sub -> {
+        log.info("messageId: " + sub.messageId());
+        log.info("grantedQoSLevels: " + sub.grantedQoSLevels());
+      })
+      .publishHandler(pub -> {
+        log.info("There are new message in topic: " + pub.topicName());
+        log.info("Content(as string) of the message: " + pub.payload().toString());
+        log.info("QoS: " + pub.qosLevel());
+      });
   }
 
   public static void publish(Buffer payload) {
-    if (null == client) start();
     client.publish("lot-pi", payload, MqttQoS.AT_LEAST_ONCE, false, false);
   }
 
